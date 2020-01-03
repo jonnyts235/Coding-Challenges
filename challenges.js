ea837eaba0e5b6a9d0203759b084c1b74aa6338b
Jonny Smith <jonnyts235@gmail.com>
2020-01-03 14:32:02 -0700

    Weighted Lottery Function in Javascript

--- challenges.js
+++ challenges.js
@@ -45,3 +45,26 @@ const addingFunction = () => {
 };
 
 addingFunction();
+
+/* Weighted Lottery Function */
+
+const weightedLottery = weights => {
+  // keep track of your  weights
+  // ['green', 'yellow', 'yellow', 'red', 'red', 'red']
+  let containerArray = [];
+
+  Object.keys(weights).forEach(key => {
+    for (let i = 0; i < weights[key]; i++) {
+      containerArray.push(key);
+    }
+  });
+
+  return containerArray[(Math.random() * containerArray.length) | 0];
+};
+
+const weights = {
+  winning: 1,
+  losing: 1000
+};
+
+weightedLottery(weights);
