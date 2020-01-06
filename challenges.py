0dc2b73548b44c4d0a9176d3704a6d9e01f30534
Jonny Smith <jonnyts235@gmail.com>
2020-01-06 11:35:16 -0700

    Bottega Diner in Python

--- challenges.py
+++ challenges.py
@@ -239,3 +239,67 @@ def zipper():
   print("".join(str(array)))
   
 zipper()
+
+""" Diner in Python """
+
+def diner():
+  main_menu = {
+    "pizza": ["Pizza", 2],
+    "tacos": ["Tacos", 2.50]
+  }
+
+  side_menu = {
+    "fries": ["Fries", 1],
+    "tatertots": ["Tatertots", 1.50]
+  }
+
+  bill = 0
+
+  # Display the menus
+  # Choose from the menus
+  # Total their bill
+
+  def greeting():
+    print("Welcome to the Python Diner!")
+
+  def menu_display(menu):
+    for item in menu.values():
+      print(f"{item[0]} => ${item[1]}")
+
+  def user_selection(menu):
+    while True:
+      user_input = input("\nWhat item would you like? ").lower()
+      menu_selection = menu.get(user_input, False)
+      if menu_selection == False:
+        print("I'm sorry, but that was not on the menu...")
+        continue
+      else:
+        print(f"You picked {menu_selection[0]}, great choice!")
+        return menu_selection[1]
+
+  def checkout():
+    print(f"\nYour bill today will be ${bill:.2f}")
+    print("\nThank you for dining at the Python Diner! Have a great day!")
+
+  greeting()
+
+  print("\nHere is the main menu:")
+  menu_display(main_menu)
+
+  bill += user_selection(main_menu)
+
+  print("\nHere is the side menu:")
+  menu_display(side_menu)
+
+  bill += user_selection(side_menu)
+
+  print("\nWhat would you like for your second side?")
+  menu_display(side_menu)
+
+  bill += user_selection(side_menu)
+
+  checkout()
+
+
+
+diner()
